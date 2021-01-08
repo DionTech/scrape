082ea196c383f1135f992e568c0f0496b64b8e6e
Daniel Koch <daniel.koch@publicare.dem>
2021-01-08 19:43:29 +0100

    some more prestructures

--- main.go
+++ main.go
@@ -7,12 +7,26 @@ import (
 )
 
 func main() {
+	var help bool
+	flag.BoolVar(&help, "help", false, "show help")
+
+	var outputDir string
+	flag.StringVar(&outputDir, "output-directory", "./scrape", "where to store scrape")
+
+	//options: RateLimit, Threads
+
 	flag.Parse()
 
 	domain := flag.Arg(0)
 
+	if domain == "" || help {
+		flag.PrintDefaults()
+		return
+	}
+
 	scrapeDefintion := scrape.ScrapeDefintion{
-		Domain: domain}
+		Domain:    domain,
+		OutputDir: outputDir}
 
 	if !scrapeDefintion.Validate() {
 		return
--- pckg/scrape/scrape.go
+++ pckg/scrape/scrape.go
@@ -1,20 +1,39 @@
 package scrape
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
+	"os"
+	"time"
 
 	"github.com/DionTech/stdoutformat"
 )
 
 type ScrapeDefintion struct {
-	Domain string
-	Index  map[string]bool
+	Domain    string
+	OutputDir string
+	Threads   int
+	RateLimit time.Duration
+	Index     map[string]bool
 }
 
 func (scrapeDefintion *ScrapeDefintion) Validate() bool {
+	if scrapeDefintion.Threads == 0 {
+		scrapeDefintion.Threads = 1
+	}
+
+	if scrapeDefintion.RateLimit == 0 {
+		scrapeDefintion.RateLimit = time.Duration(500) * time.Millisecond
+	}
+
 	if _, err := url.ParseRequestURI(scrapeDefintion.Domain); err != nil {
 		stdoutformat.Error(err)
+		return false
+	}
+
+	if scrapeDefintion.OutputDir == "" {
+		stdoutformat.Error(errors.New("OutputDir must be set"))
 		return false
 	}
 
@@ -22,5 +41,18 @@ func (scrapeDefintion *ScrapeDefintion) Validate() bool {
 }
 
 func (scrapeDefintion *ScrapeDefintion) Scrape() {
+	scrapeDefintion.mkOutPutDir()
+
 	fmt.Println(scrapeDefintion)
+}
+
+func (scrapeDefintion *ScrapeDefintion) mkOutPutDir() {
+	//check if to have to make a dir
+	if _, err := os.Stat(scrapeDefintion.OutputDir); os.IsNotExist(err) {
+		err := os.Mkdir(scrapeDefintion.OutputDir, 0755)
+
+		if err != nil {
+			stdoutformat.Fatalf("cannot create output dir %s", scrapeDefintion.OutputDir)
+		}
+	}
 }
